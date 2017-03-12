a61c122791e81f9dddb0c8fdee9c0a825ec2a5f3
Sean Kelly <kelly@seankelly.biz>
2017-03-12 15:35:30 -0500

    utility to return a sequence of brains given a sequence of relation values

--- src/jpl/mcl/site/knowledge/_utils.py
+++ src/jpl/mcl/site/knowledge/_utils.py
@@ -23,6 +23,20 @@ def publish(context, wfTool=None):
             publish(subItem, wfTool)
 
 
+def getReferencedBrains(relations):
+    u'''Given a ``relations`` sequence of RelationValue objects, return a sorted sequence
+    of matching catalog brains for thoes objects.
+    '''
+    catalog = plone.api.portal.get_tool('portal_catalog')
+    results = []
+    for rel in relations:
+        if rel.isBroken(): continue
+        brains = catalog(path={'query': rel.to_path, 'depth': 0})
+        results.append(brains[0])
+    results.sort(lambda a, b: cmp(a['Title'].decode('utf-8'), b['Title'].decode('utf-8')))
+    return results
+
+
 class IngestResults(object):
     def __init__(self, created, updated, deleted):
         self.created, self.updated, self.deleted = created, updated, deleted
