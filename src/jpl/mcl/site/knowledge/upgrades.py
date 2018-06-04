c0c82278c744c6b41d488eb8b2a139da7d791f4b
Sean Kelly <kelly@seankelly.biz>
2018-06-04 04:54:32 -0500

    get rid of imports that fail since this package does not have the required dependencies. Seriously how did this ever work?

--- src/jpl/mcl/site/knowledge/upgrades.py
+++ src/jpl/mcl/site/knowledge/upgrades.py
@@ -3,30 +3,17 @@
 u'''MCL — custom upgrade steps.'''
 
 
-from plone.app.ldap.engine.interfaces import ILDAPConfiguration
-from plone.app.ldap.engine.schema import LDAPProperty
-from plone.app.ldap.ploneldap.util import guaranteePluginExists
-from plone.registry.interfaces import IRegistry
-from Products.CMFCore.utils import getToolByName
-from Products.CMFPlone.interfaces import INavigationSchema
-from Products.PluggableAuthService.interfaces.plugins import (
-    IAuthenticationPlugin, IPropertiesPlugin, IUserAdderPlugin, IUserEnumerationPlugin, IRolesPlugin,
-    IRoleEnumerationPlugin, IGroupsPlugin, IGroupEnumerationPlugin, ICredentialsResetPlugin
-)
-from Products.PlonePAS.interfaces.plugins import IUserManagement
-from Products.PlonePAS.interfaces.group import IGroupManagement, IGroupIntrospection
-from zope.component import getUtility
-import plone.api, socket, logging, os
-from ._utils import publish, hideTab, rename, move
+from ._utils import move
+import plone.api
 
-_logger = logging.getLogger(__name__)
 
 def nullUpgradeStep(context):
     u'''Null upgrade step does nothing for when no custom behavior is needed.'''
     pass
 
+
 def movePublications(context):
     u'''Install jpl.mcl.site.knowledge.'''
-    portal = _getPortal(context)
+    portal = plone.api.portal.get()
     if 'publications' in portal['resources'].keys():
         move(portal['resources']['publications'], portal)
